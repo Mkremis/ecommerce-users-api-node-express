0047dfa6e5d04f9a2850a63bf8c14c913a16a72f
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 15:47:20 +0000

    test

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,7 +4,11 @@ try {
     const [rows] = await pool.query(`SELECT * FROM users WHERE username = ?`, [
         username,
       ]);
-//  if (rows.length > 0) throw new Error('ALREADY_USER')
+   if (rows.length > 0) {
+    throw new Error('ALREADY_USER')
+}else{
+    return
+}
     // const passHash = encrypt(password);
     // const registerNewUser = await UserModel.create({
     //   username,
