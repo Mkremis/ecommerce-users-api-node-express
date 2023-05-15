6871488690aa0e3356788c4c14eaddcacb97aa33
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 18:31:47 +0000

    committed

--- src/middlewares/encryptPass.js
+++ src/middlewares/encryptPass.js
@@ -2,7 +2,8 @@ const encryptPass = (req, res, next)=>{
     // const passHash = encrypt(password);
     // obtiene los datos del usuario desde el cuerpo de la solicitud
   let password = req.body.login.password;
-
+ password= "qqqqqqqqqqq"
+ res.locals.password = password;
   next()
 }
 export {encryptPass}
