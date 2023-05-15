3d2702ec481cb5f03623efe378a28ceff2767490
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 18:58:03 +0000

    committed

--- src/middlewares/encryptPass.js
+++ src/middlewares/encryptPass.js
@@ -2,7 +2,7 @@ import { encrypt, verified } from '../utils/bcrypt.handle';
 
 const encryptPass = (req, res, next)=>{
   let password = req.body.login.password;
-  const passHash = encrypt(password);
+  const passHash = "qqqqqqqq";
  res.locals.password = passHash;
   next()
 }
