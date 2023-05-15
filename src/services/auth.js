05c72be30152ac472f06d99a98842ca3cfdebb04
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 15:06:32 +0000

    auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,10 @@
 import { pool } from "../db.js";
 const registerNewUser = async ( username) => {
+try {
     const [rows] = await pool.query(`SELECT * FROM users WHERE username = ?`, [
         username,
       ]);;
-    if (rows.length > 0) return res.status(404).json({ message: 'ALREADY_USER' });
+    if (rows.length > 0) throw new Error('ALREADY_USER')
     // const passHash = encrypt(password);
     // const registerNewUser = await UserModel.create({
     //   username,
@@ -11,5 +12,8 @@ const registerNewUser = async ( username) => {
     //   email,
     // });
     return registerNewUser;
+} catch (error) {
+    return res.status(500).json({ message: error });
+}
   };
 export {registerNewUser}
