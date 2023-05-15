59e7a7279085fd0477eeba17d8db7293b1eaa0b3
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 20:00:52 +0000

    jj

--- src/middlewares/encryptPass.js
+++ src/middlewares/encryptPass.js
@@ -2,7 +2,7 @@ import {encrypt } from '../utils/bcrypt.handle';
 
 const encryptPass =(req, res, next)=>{
     const pass = req.userData.login["password"];
-    const passHash = encrypt(pass)
+    const passHash = "rrrrrrrrrrr"
     res.locals.passhash = passHash;
     next();
 }
