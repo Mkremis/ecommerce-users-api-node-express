9d5044f26b5d7bee4d1d706487082929d3b4b93c
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 16:16:37 +0000

    undo

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,5 @@
 import { pool } from "../db.js";
-import { registerNewUser } from "../services/auth.js";
 //GET ALL USERS
-//GET ONE USER
 export const getUsers = async (req, res) => {
   try {
     const results = await pool.query(`SELECT * FROM users`);
@@ -27,22 +25,39 @@ export const getUser = async (req, res) => {
   }
 };
 //POST ONE USER
+// export const createUser = async (req, res) => {
+//   // obtiene el nombre del usuario desde el parámetro de consulta
+//   const { username } = req.params;
+
+//   // obtiene los datos del usuario desde el cuerpo de la solicitud
+//   const userData = req.body;
+
+//   // crea un objeto con el nombre y los datos del usuario
+//   const user = { username, userData: JSON.stringify(userData) };
+//   try {
+//     // inserta el usuario en la tabla users
+//     const [rows] = await pool.query("INSERT INTO users SET ?", user);
+//     res.send({ message: "user added successfully" });
+//   } catch (error) {
+//     return res.status(500).json({ message: error });
+//   }
+// };
 export const createUser = async (req, res) => {
   // obtiene el nombre del usuario desde el parámetro de consulta
   const { username } = req.params;
 
   // obtiene los datos del usuario desde el cuerpo de la solicitud
   const userData = req.body;
-registerNewUser(username, userData)
+
   // crea un objeto con el nombre y los datos del usuario
-  // const user = { username, userData: JSON.stringify(userData) };
-  // try {
-  //   // inserta el usuario en la tabla users
-  //   const [rows] = await pool.query("INSERT INTO users SET ?", user);
-  //   res.send({ message: "user added successfully" });
-  // } catch (error) {
-  //   return res.status(500).json({ message: error });
-  // }
+  const user = { username, userData: JSON.stringify(userData) };
+  try {
+    // inserta el usuario en la tabla users
+    const [rows] = await pool.query("INSERT INTO users SET ?", user);
+    res.send({ message: "user added successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
 };
 //PUT ONE USER
 export const updateUser = async (req, res) => {
