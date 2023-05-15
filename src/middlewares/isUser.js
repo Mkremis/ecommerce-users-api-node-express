76c632b15320448daebec457ede91529892cc947
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-15 21:12:56 +0000

    v

--- src/middlewares/isUser.js
+++ src/middlewares/isUser.js
@@ -1,5 +1,5 @@
 import { pool } from "../db.js";
-
+import {hashSync} from 'bcryptjs'
 const isUser = async (req, res, next) => {
 
        // obtiene el nombre del usuario desde el parámetro de consulta
@@ -11,7 +11,7 @@ const isUser = async (req, res, next) => {
    if (rows.length > 0) {
     return res.status(404).json({ message: "ALREADY_USER" });
 }else{
-
+ const hash = hashSync('bacon', 8);
     next()
 }
   };
