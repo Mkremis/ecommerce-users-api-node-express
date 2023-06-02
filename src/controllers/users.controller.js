6cc1a96fb97ce89054d01c143b453a929d43b9bb
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-02 22:54:19 +0000

    commit

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -31,7 +31,8 @@ export const createUser = async (req, res) => {
 
   try {
     // inserta el usuario en la tabla users
-    const [rows] = await pool.query("INSERT INTO users (login_password, login_username, fullname_first, fullname_last, contact_email, contact_phone, picture_thumbnail, fullname_title, location_city, location_state, location_number, location_street, location_country, location_postcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [userData.login_username, userData.login_password, userData.fullname_first, userData.fullname_last, userData.contact_email, userData.contact_phone, userData.picture_thumbnail, userData.fullname_title, userData.location_city, userData.location_state, userData.location_number, userData.location_street, userData.location_country, userData.location_postcode]);
+    const [rows] = await pool.query("INSERT INTO users (login_password, login_username, fullname_title, fullname_first, fullname_last, contact_email, contact_phone, picture_thumbnail, location_city, location_state, location_number, location_street, location_country, location_postcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+     [userData.login_password, userData.login_username, userData.fullname_title, userData.fullname_first, userData.fullname_last, userData.contact_email, userData.contact_phone, userData.picture_thumbnail, userData.location_city, userData.location_state, userData.location_number, userData.location_street, userData.location_country, userData.location_postcode]);
 
     res.send({ message: "user added successfully" });
   } catch (error) {
