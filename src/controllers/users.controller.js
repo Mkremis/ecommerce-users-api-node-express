8e8c204df68cbed30cca7ae6a3514f2a88518a28
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-02 16:28:45 +0000

    error message

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,7 +6,7 @@ export const getUsers = async (req, res) => {
     const results = await pool.query(`SELECT * FROM users`);
     res.json(results[0]);
   } catch (error) {
-    return res.status(500).json({ message: "something goes wrong" });
+    return res.status(500).json({ message: error });
   }
 };
 //GET ONE USER
@@ -14,13 +14,12 @@ export const getUser = async (req, res) => {
   const { username } = req.params;
 
   try {
-    const [rows] = await pool.query(`SELECT * FROM users WHERE username = ?`, [
+    const [rows] = await pool.query(`SELECT * FROM users WHERE login_username = ?`, [
       username,
     ]);
     if (rows.length <= 0)
       return res.status(404).json({ message: "user not found" });
     res.json(rows[0]);
-    console.log(rows[0]);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
