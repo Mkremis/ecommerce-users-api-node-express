17ed5ce2eadaf266b1c0b5dd2bf3c92d2b345a3a
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-04 17:23:02 +0000

    commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,6 +4,7 @@ import { generateToken } from "../utils/jwtHandle.js";
 
 const registerNewUser = async ({ userData }) => {
   userData.login_password = await encrypt(userData.login_password);
+
   try {
     // inserta el usuario en la tabla users
     const [rows] = await pool.query(
@@ -40,91 +41,19 @@ const loginUser = async ({ login_username, login_password, passwordHash }) => {
     login_username
   );
   const data = { token, user: rows[0] };
+
   return data;
 };
 
 const getData = async ({ username }) => {
+ 
   const [rows] = await pool.query(
     `SELECT * FROM users WHERE login_username = ?`,
     username
   );
   const data = { user: rows[0] };
+
   return data;
 };
 
-// const updateDataUser = async ({ userData }) => {
-//   try {
-//     const [rows] = await pool.query(
-//       "UPDATE users (login_password, login_username, fullname_title, fullname_first, fullname_last, contact_email, contact_phone, picture_thumbnail, location_city, location_state, location_number, location_street, location_country, location_postcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) WHERE login_username = userData.login_username",
-//       [
-//         userData.login_password,
-//         userData.login_username,
-//         userData.fullName_title,
-//         userData.fullName_first,
-//         userData.fullName_last,
-//         userData.contact_email,
-//         userData.contact_phone,
-//         userData.picture_thumbnail,
-//         userData.location_city,
-//         userData.location_state,
-//         userData.location_number,
-//         userData.location_street,
-//         userData.location_country,
-//         userData.location_postcode,
-//       ]
-//     );
-//     if (rows.affectedRows) return { success: rows };
-//   } catch (error) {
-//     return { fail: error };
-//   }
-// };
-
-const updateDataUser = async ({ userData }) => {
-  try {
-    const updateQuery = `
-      UPDATE users 
-      SET 
-        login_password = ?,
-        fullname_title = ?,
-        fullname_first = ?,
-        fullname_last = ?,
-        contact_email = ?,
-        contact_phone = ?,
-        picture_thumbnail = ?,
-        location_city = ?,
-        location_state = ?,
-        location_number = ?,
-        location_street = ?,
-        location_country = ?,
-        location_postcode = ?
-      WHERE login_username = ?
-    `;
-    
-    const [rows] = await pool.query(updateQuery, [
-      userData.login_password,
-      userData.login_username,
-      userData.fullName_title,
-      userData.fullName_first,
-      userData.fullName_last,
-      userData.contact_email,
-      userData.contact_phone,
-      userData.picture_thumbnail,
-      userData.location_city,
-      userData.location_state,
-      userData.location_number,
-      userData.location_street,
-      userData.location_country,
-      userData.location_postcode,
-      userData.login_username, // Agregado el valor para el WHERE
-    ]);
-
-    if (rows.affectedRows) {
-      return { success: rows };
-    }
-  } catch (error) {
-    return { fail: error };
-  }
-};
-
-
-export { registerNewUser, loginUser, getData, updateDataUser };
+export { registerNewUser, loginUser, getData };
