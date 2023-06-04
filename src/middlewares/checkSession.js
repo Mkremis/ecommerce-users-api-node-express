ee96c0e9edf55edc1523ee6b8de0dce8203faca6
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-04 19:26:50 +0000

    commit

--- src/middlewares/checkSession.js
+++ src/middlewares/checkSession.js
@@ -1,24 +1,24 @@
+import { verifyToken } from "../utils/jwtHandle";
 
+verifyToken
 
 const checkSession = (req, res, next) => {
-  res.status(401);
-  res.json({message: 'NO_TIENES_UNA_SESSION_VALIDA'});
-  // try {
-  //   const jwtByUser = req.headers.authorization || '';
-  //   const jwt = jwtByUser.split(' ').pop(); //['Bearer','11111']
-  //   const isUser = verifyToken(`${jwt}`);
-  //   console.log('isUser', isUser);
-  //   if (!isUser) {
-  //     res.status(401);
-  //     res.json({message: 'NO_TIENES_UNA_SESSION_VALIDA'});
-  //   } else {
-  //     req.user = isUser;
-  //     next();
-  //   }
-  // } catch (e) {
-  //   res.status(400);
-  //   console.log(e);
-  //   res.json({message: 'SESSION_VALIDA'});
-  // }
+  try {
+    const jwtByUser = req.headers.authorization || '';
+    const jwt = jwtByUser.split(' ').pop(); //['Bearer','11111']
+    const isUser = verifyToken(`${jwt}`);
+    console.log('isUser', isUser);
+    if (!isUser) {
+      res.status(401);
+      res.json({message: 'NO_TIENES_UNA_SESSION_VALIDA'});
+    } else {
+      req.user = isUser;
+      next();
+    }
+  } catch (e) {
+    res.status(400);
+    console.log(e);
+    res.json({message: 'SESSION_VALIDA'});
+  }
 };
 export { checkSession };
