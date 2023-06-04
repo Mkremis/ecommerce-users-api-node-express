c69cdeab026fc538d6271834196fba54613745b1
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-04 17:53:42 +0000

    test

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -43,11 +43,12 @@ export const register = async (req, res) => {
 
 //PUT ONE USER
 export const updateUser = async (req, res) => {
-  let userData = req.body;
-  const responseUpdate = await updateUserData({ userData });
-  if (responseUpdate.success)
-    return res.status(200).send(responseUpdate.success);
-  return res.status(500).json(responseUpdate.fail);
+  res.json({test: 'hola'})
+  // let userData = req.body;
+  // const responseUpdate = await updateUserData({ userData });
+  // if (responseUpdate.success)
+  //   return res.status(200).send(responseUpdate.success);
+  // return res.status(500).json(responseUpdate.fail);
 };
 // DELETE ONE USER
 export const deleteUser = async (req, res) => {
