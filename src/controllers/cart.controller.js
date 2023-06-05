ab713f72b984b1506c614b3bdd12af613bc75cf8
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-05 17:53:55 +0000

    TEST

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -18,14 +18,13 @@ export const updateCart = async (req, res) => {
   const { username } = req.params;
   const cart = req.body;
   // UPDATE cart SET user_cart = '{}' WHERE login_username = 'elvyspresley';
-  return res.json({[username]:cart})
-  // try {
-  //   const [result] = await pool.query(
-  //     "UPDATE cart SET user_cart = ? WHERE login_username = ?",
-  //     [cart, username]
-  //   );
-  //   res.json({ message: result.info });
-  // } catch (error) {
-  //   return res.status(500).json({ message: error });
-  // }
+  try {
+    const [result] = await pool.query(
+      "UPDATE cart SET user_cart = ? WHERE login_username = ?",
+      [cart, username]
+    );
+    res.json({ message: result.info });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
 };
