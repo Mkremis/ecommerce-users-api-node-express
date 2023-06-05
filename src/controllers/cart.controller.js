8dd47072269956e0b4de5041c1a40b173d2bd8c1
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-05 18:06:47 +0000

    commit

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -18,13 +18,14 @@ export const updateCart = async (req, res) => {
   const { username } = req.params;
   const cart = req.body;
   // UPDATE cart SET user_cart = '{}' WHERE login_username = 'elvyspresley';
-  try {
-    const [result] = await pool.query(
-      "UPDATE cart SET user_cart = ? WHERE login_username = ?",
-      [cart, username]
-    );
-    res.json({ message: result.info });
-  } catch (error) {
-    return res.status(500).json({ message: error });
-  }
+  return res.json({[username]:cart})
+  // try {
+  //   const [result] = await pool.query(
+  //     "UPDATE cart SET user_cart = ? WHERE login_username = ?",
+  //     [cart, username]
+  //   );
+  //   res.json({ message: result.info });
+  // } catch (error) {
+  //   return res.status(500).json({ message: error });
+  // }
 };
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -7,6 +7,6 @@ const router = Router();
 
 router
   .get("/users/:username/cart", getCart)
-  .put("/users/:username/update-cart", updateCart)
+  .post("/users/:username/update-cart", updateCart)
 
 export default router;
