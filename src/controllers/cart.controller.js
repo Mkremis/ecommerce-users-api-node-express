821e9d3f0cff047bdce84d6c4b8292fd27be822e
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-05 17:24:58 +0000

    json

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -7,7 +7,7 @@ export const getCart = async (req, res) => {
     const [rows] = await pool.query(`SELECT user_cart FROM cart WHERE login_username = ?`, [
       username,
     ]);
-    res.json(rows[0]);
+    res.json({message:rows[0]});
   } catch (error) {
     return res.status(500).json({ message: error });
   }
@@ -15,9 +15,7 @@ export const getCart = async (req, res) => {
 
 //PUT CART
 export const updateCart = async (req, res) => {
-  // obtiene el nombre del usuario desde el parámetro de consulta
   const { username } = req.params;
-  // obtiene el cart del usuario desde el cuerpo de la solicitud
   const cart = req.body;
   try {
     const [result] = await pool.query(
