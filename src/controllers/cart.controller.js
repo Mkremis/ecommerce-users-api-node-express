887dffbe667fab976349690f09d709c4930b71c2
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-05 17:16:50 +0000

    update cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -7,10 +7,7 @@ export const getCart = async (req, res) => {
     const [rows] = await pool.query(`SELECT user_cart FROM cart WHERE login_username = ?`, [
       username,
     ]);
-    // if (rows.length = 0)
-    //   return res.status(404).json({ message: "user not found" });
     res.json(rows[0]);
-    console.log(rows[0]);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
@@ -21,16 +18,12 @@ export const updateCart = async (req, res) => {
   // obtiene el nombre del usuario desde el parámetro de consulta
   const { username } = req.params;
   // obtiene el cart del usuario desde el cuerpo de la solicitud
-  const cart = JSON.stringify(req.body);
-  // crea un objeto con el nombre y el cart del usuario
-
+  const cart = req.body;
   try {
     const [result] = await pool.query(
-      "UPDATE users SET cart = ? WHERE username = ?",
+      "UPDATE cart SET user_cart = ? WHERE login_username = ?",
       [cart, username]
     );
-    if (result.affectedRows === 0)
-      return res.status(404).json({ message: "user not found" });
     res.json({ message: result.info });
   } catch (error) {
     return res.status(500).json({ message: error });
