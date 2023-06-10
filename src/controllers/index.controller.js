857011105c7ad724a09af699b05d31f4573f9acf
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-10 19:34:53 +0000

    Update 4 files

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,3 +4,13 @@ export const ping = async (req, res) => {
   const [result] = await pool.query("SELECT 'pong' AS result");
   res.json(result[0]);
 };
+
+export const tables = async (req, res) => {
+  const [result] = await pool.query("SHOW TABLES");
+  res.json(result[0]);
+};
+
+export const users = async (req, res) => {
+  const [result] = await pool.query("DESCRIBE users");
+  res.json(result);
+};
