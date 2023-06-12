86e76f77f96b0441d205e2c3cd0957289539b98a
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-12 17:20:02 +0000

    Update sales.js

--- src/services/sales.js
+++ src/services/sales.js
@@ -2,15 +2,15 @@ import { pool } from "../db.js";
 
 export const registerSale = async (items, username, date, feeType) => {
   const query = `UPDATE sales SET(
-        login_username = ?,
-        transactionType = ?,
-        transactionDate = ?,
-        gender = ?,
-        prodName = ?,
-        productQ = ?,
-        prodImage = ?,
-        prodPrice = ?,
-        WHERE login_username = ?`;
+    login_username = ?,
+    transactionType = ?,
+    transactionDate = ?,
+    gender = ?,
+    prodName = ?,
+    productQ = ?,
+    prodImage = ?,
+    prodPrice = ?,
+    WHERE login_username = ?`;
 
   items.map(async (item) => {
     const [rows] = await pool.query(query, [
