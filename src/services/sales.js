663bfc91f244c4e6dc41552b193b8bdc794ab281
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-12 18:01:37 +0000

    Update payment.controller.js and sales.js

--- src/services/sales.js
+++ src/services/sales.js
@@ -1,37 +1,38 @@
 import { pool } from "../db.js";
 
 export const registerSale = async (items, username, date, feeType) => {
- const query = `UPDATE sales SET login_username=?, transactionType=?, transactionDate=?, gender=?, prodName=?, productQ=?, prodImage=?, prodPrice=? WHERE login_username=?`;
-
   items.map(async (item) => {
-    const [rows] = await pool.query(query, [
-      username,
-      feeType,
-      new Date(date),
-      item.category_id,
-      item.title,
-      parseInt(item.quantity),
-      item.picture_url,
-      parseFloat(item.unit_price),
-      username, // Agregado el valor para el WHERE
-    ]);
-    console.log("registerSale", rows);
+    await pool.query(
+      `INSERT INTO sales (
+        login_username,
+        transactionType,
+        transactionDate,
+        gender,
+        prodId,
+        prodName,
+        productQ,
+        prodImage,
+        prodPrice)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        username,
+        feeType,
+        new Date(date),
+        item.category_id,
+        item.id,
+        item.title,
+        parseInt(item.quantity),
+        item.picture_url,
+        parseFloat(item.unit_price),
+      ]
+    );
   });
 };
 
-// items.map(async (item) => {
-//   await pool.query(
-//     `INSERT INTO sales (
-//         login_username,
-//         transactionType,
-//         transactionDate,
-//         gender,
-//         prodName,
-//         productQ,
-//         prodImage,
-//         prodPrice)
-//         VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-//     [
+//  const query = `UPDATE sales SET login_username=?, transactionType=?, transactionDate=?, gender=?, prodName=?, productQ=?, prodImage=?, prodPrice=? WHERE login_username=?`;
+
+//   items.map(async (item) => {
+//     const [rows] = await pool.query(query, [
 //       username,
 //       feeType,
 //       new Date(date),
@@ -40,6 +41,7 @@ export const registerSale = async (items, username, date, feeType) => {
 //       parseInt(item.quantity),
 //       item.picture_url,
 //       parseFloat(item.unit_price),
-//     ]
-//   );
-// });
+//       username, // Agregado el valor para el WHERE
+//     ]);
+//     console.log("registerSale", rows);
+//   });
