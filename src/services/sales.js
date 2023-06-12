9fdbfb3a6211cfd1067b38913214d4e3f5e144a8
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-12 17:12:35 +0000

    Update sales.js

--- src/services/sales.js
+++ src/services/sales.js
@@ -1,28 +1,30 @@
 import { pool } from "../db.js";
 
 export const registerSale = async (items, username, date, feeType) => {
+  const query = `UPDATE sales SET(
+        login_username = ?,
+        transactionType = ?,
+        transactionDate = ?,
+        gender = ?,
+        prodName = ?,
+        productQ = ?,
+        prodImage = ?,
+        prodPrice = ?,
+        WHERE login_username = ?`;
+
   items.map(async (item) => {
-    await pool.query(
-      `UPDATE sales SET(
-          transactionType,
-          transactionDate,
-          gender,
-          prodName,
-          productQ,
-          prodImage,
-          prodPrice) 
-          VALUES (?, ?, ?, ?, ?, ?, ?) WHERE login_username = ?`,
-      [
-        feeType,
-        new Date(date),
-        item.category_id,
-        item.title,
-        parseInt(item.quantity),
-        item.picture_url,
-        parseFloat(item.unit_price),
-        username,
-      ]
-    );
+    const [rows] = await pool.query(query, [
+      username,
+      feeType,
+      new Date(date),
+      item.category_id,
+      item.title,
+      parseInt(item.quantity),
+      item.picture_url,
+      parseFloat(item.unit_price),
+      username, // Agregado el valor para el WHERE
+    ]);
+    console.log("registerSale", rows);
   });
   // items.map(async (item) => {
   //   await pool.query(
