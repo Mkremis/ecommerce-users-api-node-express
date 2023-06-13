0430c529da076a0442474959247442e7a11ee84f
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-13 19:39:50 +0000

    Update 3 files

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import { checkSession } from "../middlewares/checkSession.js";
-import {
-  getCart,
-  updateCart,
-} from "../controllers/cart.controller.js";
+import { getCart, updateCart } from "../controllers/cart.controller.js";
 const router = Router();
 
 router
-  .get("/users/:username/cart", getCart)
-  .put("/users/:username/update-cart", checkSession, updateCart)
+  .get("/users/:username/cart", checkSession, getCart)
+  .put("/users/:username/update-cart", checkSession, updateCart);
 
 export default router;
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,14 +1,25 @@
 import { pool } from "../db.js";
 
-export const cartUpdate = async ({username, cart})=>{ 
-    try {
-        const [rows] = await pool.query(
-            "UPDATE users SET user_cart = ? WHERE login_username = ?",
-            [cart, username]
-          );
-        if (rows.affectedRows) return { success: rows };
-    } catch (error) {
-      return { fail: error };
-    }
+export const cartUpdate = async ({ username, cart }) => {
+  try {
+    const [rows] = await pool.query(
+      "UPDATE users SET user_cart = ? WHERE login_username = ?",
+      [cart, username]
+    );
+    if (rows.affectedRows) return { success: rows };
+  } catch (error) {
+    return { fail: error };
+  }
 };
 
+export const getUserCart = async ({ username }) => {
+  try {
+    const [rows] = await pool.query(
+      `SELECT user_cart FROM users WHERE login_username = ?`,
+      username
+    );
+    if (rows.affectedRows) return { success: rows };
+  } catch (error) {
+    return { fail: error };
+  }
+};
