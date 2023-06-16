a35c1629747a05d1f913284ac906a7dab503079b
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-16 22:27:07 +0000

    Update checkSession.js

--- src/middlewares/checkSession.js
+++ src/middlewares/checkSession.js
@@ -5,13 +5,16 @@ const checkSession = (req, res, next) => {
     const jwtByUser = req.headers.authorization || "";
     const jwt = jwtByUser.split(" ").pop(); //['Bearer','11111']
     const isUser = verifyToken(`${jwt}`);
-    if (isUser) {
+    if (!isUser) {
+      res.status(401);
+      res.send("NO_TIENES_UNA_SESSION_VALIDA");
+    } else {
       req.user = isUser;
       next();
     }
   } catch (error) {
     res.status(400);
-    res.json({ message: "NO_TIENES_UNA_SESSION_VALIDA" });
+    res.json(error);
   }
 };
 export { checkSession };
