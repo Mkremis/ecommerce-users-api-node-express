0270fd6f310a82a552e293e29298d49cd52109d3
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-16 20:11:19 +0000

    Update users.controller.js and auth.services.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -11,49 +11,59 @@ export const getUserData = async (req, res) => {
     const { username } = req.params;
     const responseData = await getData({ username });
     if (responseData.success) return res.status(200).json(responseData.success);
-    return res.status(404).json(responseData.error);
+    return res.status(404).json(responseData.fail);
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ error });
   }
 };
 
 //Login
 export const login = async (req, res) => {
-  if (!req.passwordHash)
-    return res.status(404).json({ message: "NOT_FOUND_USER" });
-  const { body } = req;
-  const { login_username, login_password } = body;
-  const { passwordHash } = req;
-  const responseUser = await loginUser({
-    login_username,
-    login_password,
-    passwordHash,
-  });
-  if (responseUser === "INCORRECT_PASSWORD") {
-    res.status(403);
-    res.json({ message: responseUser });
-  } else {
-    res.json(responseUser);
+  try {
+    if (!req.passwordHash)
+      return res.status(404).json({ message: "NOT_FOUND_USER" });
+    const { body } = req;
+    const { login_username, login_password } = body;
+    const { passwordHash } = req;
+    const responseUser = await loginUser({
+      login_username,
+      login_password,
+      passwordHash,
+    });
+    if (responseUser === "INCORRECT_PASSWORD") {
+      res.status(403).json({ message: responseUser });
+    } else {
+      res.json(responseUser);
+    }
+  } catch (error) {
+    res.status(500).json({ error });
   }
 };
 
 //register
 export const register = async (req, res) => {
-  if (req.passwordHash)
-    return res.status(409).json({ message: "ALREADY_USER" });
-  // obtiene los datos del usuario desde el cuerpo de la solicitud
-  let userData = req.body;
-  const responseRegister = await registerNewUser({ userData });
-  if (responseRegister.success)
-    return res.status(200).json(responseRegister.success);
-  return res.status(500).json(responseRegister.fail);
+  try {
+    if (req.passwordHash)
+      return res.status(409).json({ message: "ALREADY_USER" });
+    let userData = req.body;
+    const responseRegister = await registerNewUser({ userData });
+    if (responseRegister.success)
+      return res.status(200).json(responseRegister.success);
+    return res.status(409).json(responseRegister.fail);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 //UPDATE USER
 export const updateUser = async (req, res) => {
-  let userData = req.body;
-  const responseUpdate = await updateUserData({ userData });
-  if (responseUpdate.success)
-    return res.status(200).json(responseUpdate.success);
-  return res.status(500).json(responseUpdate.fail);
+  try {
+    let userData = req.body;
+    const responseUpdate = await updateUserData({ userData });
+    if (responseUpdate.success)
+      return res.status(200).json(responseUpdate.success);
+    return res.status(500).json(responseUpdate.fail);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
