d5a1dd0767b7ef390898bed4c89b43fdbc20ddf4
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-17 00:09:31 +0000

    Update orders.routes.js

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -3,5 +3,5 @@ import { getUserOrders } from "../controllers/orders.controller.js";
 import { checkSession } from "../middlewares/checkSession.js";
 
 const router = Router();
-router.get("/orders/:username", checkSession, getUserOrders);
+router.get("/users/:username/orders", checkSession, getUserOrders);
 export default router;
