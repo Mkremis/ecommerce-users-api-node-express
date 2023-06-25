de31fd2f11c219a645633216db51bce1f57e7883
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-25 19:08:55 +0000

    Update cart.controller.js and cart.services.js

--- src/services/cart.services.js
+++ src/services/cart.services.js
@@ -1,12 +1,10 @@
 import { pool } from "../db.js";
 
 export const cartUpdate = async ({ username, cart }) => {
-  let updatedCart = cart ? JSON.stringify(cart) : null;
-  console.log("cartUpdate", updatedCart);
   try {
     const [rows] = await pool.query(
       "UPDATE users SET user_cart = ? WHERE login_username = ?",
-      [updatedCart, username]
+      [cart, username]
     );
     if (rows.affectedRows) return { success: rows };
   } catch (error) {
