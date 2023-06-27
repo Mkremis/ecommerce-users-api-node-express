5be50f80fd5aaae92a2dbd66930c1606c4eec839
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-27 19:54:09 +0000

    Update credentials.js

--- src/middlewares/credentials.js
+++ src/middlewares/credentials.js
@@ -1,4 +1,4 @@
-import { allowedOrigins } from "../config/allowedOrigins";
+import { allowedOrigins } from "../config/allowedOrigins.js";
 
 export const credentials = (req, res, next) => {
   const origin = req.headers.origin;
