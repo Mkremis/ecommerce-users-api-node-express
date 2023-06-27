bd51fe63b636cf94dece712b57c78d2cfebeb84f
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-27 18:43:13 +0000

    Update users.controller.js and auth.services.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -31,13 +31,7 @@ export const login = async (req, res) => {
     if (responseUser === "INCORRECT_PASSWORD") {
       res.status(403).json({ message: responseUser });
     } else {
-      res.json(responseUser.userData);
-      // res.cookie("jwt", responseUser.refreshToken, {
-      //   httpOnly: true,
-      //   sameSite: "None",
-      //   secure: true,
-      //   maxAge: 24 * 60 * 60 * 1000,
-      // });
+      res.json(responseUser);
     }
   } catch (error) {
     res.status(500).json({ error });
