a0b70a3df1390e3f5d4eb6c693b36f5712540611
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-28 22:00:39 +0000

    Update refreshToken.controller.js

--- src/controllers/refreshToken.controller.js
+++ src/controllers/refreshToken.controller.js
@@ -1,20 +1,20 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 import { pool } from "../db.js";
-import dotenv from ('dotenv')
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const handleRefreshToken = (req, res) => {
-  const {login_username} = req.params;
+  const { login_username } = req.params;
   const cookies = req.cookies;
-  res.json(login_username, cookies)
+  res.json(login_username, cookies);
   // if (!cookies?.jwt) return res.sendStatus(401);
   // const refreshToken = cookies.jwt;
   // let [rows] = await pool.query(
   //   `SELECT refresh_token FROM users WHERE login_username = ?`,
   //   [login_username]
   // );
- 
+
   //  if (rows.length === 0) return res.sendStatus(403); //forbidden
   // // evaluate jwt
   // jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
@@ -26,4 +26,3 @@ export const handleRefreshToken = (req, res) => {
   //   res.json({ accessToken });
   // });
 };
-
