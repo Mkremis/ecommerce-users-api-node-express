f07de04b149087fec0fd8cf2a5eccf6578febe73
Martin <105755654+Mkremis@users.noreply.github.com>
2023-07-01 20:16:30 +0000

    Update 3 files

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,1 @@
-export const allowedOrigins = [
-  "https://mkremis-studious-computing-machine-q5wg4q544x72xrrv-5173.preview.app.github.dev",
-];
+export const allowedOrigins = ["https://mkremis.github.io"];
