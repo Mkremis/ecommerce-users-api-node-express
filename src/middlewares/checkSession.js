3d28a713ce91baf11f2f5c281cc3d43999a109c3
Martin <105755654+Mkremis@users.noreply.github.com>
2023-07-01 16:38:11 +0000

    Update checkSession.js

--- src/middlewares/checkSession.js
+++ src/middlewares/checkSession.js
@@ -1,27 +1,17 @@
-import { verifyToken, refreshToken } from "../utils/jwtHandle.js";
+import { verifyToken } from "../utils/jwtHandle.js";
 
 const checkSession = (req, res, next) => {
-  // try {
-  //   const { jwt } = req.cookies;
-  //   // const jwtByUser = req.headers.authorization || "";
-  //   // const jwt = jwtByUser.split(" ").pop(); //['Bearer','11111']
-  //   const isUser = verifyToken(jwt);
-
-  //   if (isUser) {
-  //     req.user = isUser;
-  //     res.json({ user: isUser, cookie: jwt });
-  //     console.log(jwt, isUser);
-  //     //     // El token de actualización es válido, generar un nuevo token de acceso
-  //     // const freshToken = refreshToken()
-  //     // res.json({ 'jwt': newAccessToken });
-  //     next();
-  //   } else {
-  //     res.status(401).json({ message: "NO_TIENES_UNA_SESSION_VALIDA", isUser });
-  //   }
-  // } catch (error) {
-  //   res.sendStatus(500);
-  // }
-  res.status(200).json({ req });
-  next();
+  try {
+    const jwtByUser = req.headers.authorization || "";
+    const jwt = jwtByUser.split(" ").pop(); //['Bearer','11111']
+    const isUser = verifyToken(`${jwt}`);
+    if (isUser) {
+      req.user = isUser;
+      next();
+    }
+  } catch (error) {
+    res.status(401);
+    res.json({ message: "NO_TIENES_UNA_SESSION_VALIDA" });
+  }
 };
 export { checkSession };
