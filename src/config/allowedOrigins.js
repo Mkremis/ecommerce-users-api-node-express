7a5f3c077cce413ad1be8bf99342529d0415109f
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-07-01 23:04:41 +0000

    allowedOrigins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,1 +1,1 @@
-export const allowedOrigins = ["https://mkremis-studious-computing-machine-q5wg4q544x72xrrv-5173.preview.app.github.dev"];
+export const allowedOrigins = ["https://mkremis-studious-computing-machine-q5wg4q544x72xrrv-5173.preview.app.github.dev", "https://mkremis.github.io/ecommerce-react"];
