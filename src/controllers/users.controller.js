d76f7aab8d0e3dc33fd6e13ffde67c47c8984160
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-07-02 01:41:23 -0300

    Update users.controller.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,6 +7,7 @@ import {
 
 //Dashboard
 export const getUserData = async (req, res) => {
+  console.log("cookies",req.cookies)
   try {
     const { username } = req.params;
     const responseData = await getData({ username });
