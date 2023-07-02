95b0ba33d2a81473dc44d9d0bfc07723897bd627
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-07-02 16:35:45 +0000

    cookie

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -35,7 +35,7 @@ export const login = async (req, res) => {
       res.cookie('accessToken', responseUser?.accessToken, {
         httpOnly: true,
         sameSite: "None",
-        secure: true,
+        secure: false,
         maxAge: 24 * 60 * 60 * 1000,
       });
       const {accessToken, userData} = responseUser
