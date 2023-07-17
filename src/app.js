4c4e7e51c4c2cc23430d8872679edebe7bbc2612
Martin <105755654+Mkremis@users.noreply.github.com>
2023-07-17 16:10:01 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,9 +16,14 @@ import { corsOptions } from "./config/corsOptions.js";
 
 const app = express();
 app
+
   .use(cookieParser())
   // Handle options credentials check - before CORS!
   // and fetch cookies credentials requirement
+  .use((req, res, next) => {
+    console.log(req.cookies);
+    next();
+  })
   .use(credentials)
   .use(cors(corsOptions));
 
