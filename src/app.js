c0f4127db260f88f753078cea81de39a0f4a96d3
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-21 16:42:59 -0300

    1 file modified, 2 deleted

--- src/app.js
+++ src/app.js
@@ -1,38 +1,31 @@
-import express from "express";
-import cors from "cors";
-// import morgan from "morgan";
+import express from 'express';
+import cors from 'cors';
+import { corsOptions } from './config/corsOptions.js';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import { errorHandler } from './middlewares/errorHandler.js';
 
-import usersRoutes from "./routes/users.routes.js";
-import cartRoutes from "./routes/cart.routes.js";
-import indexRoutes from "./routes/index.routes.js";
-import payRoutes from "./routes/payment.routes.js";
-import orderRoutes from "./routes/orders.routes.js";
-import likesRoutes from "./routes/likes.routes.js";
-import { logger } from "./middlewares/logEvents.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
-import cookieParser from "cookie-parser";
-import { credentials } from "./middlewares/credentials.js";
-import { corsOptions } from "./config/corsOptions.js";
+import usersRoutes from './routes/users.routes.js';
+import cartRoutes from './routes/cart.routes.js';
+import indexRoutes from './routes/index.routes.js';
+import payRoutes from './routes/payment.routes.js';
+import orderRoutes from './routes/orders.routes.js';
+import likesRoutes from './routes/likes.routes.js';
 
 const app = express();
 app
   .use(cors(corsOptions))
-  // .use(credentials)
-
   .use(cookieParser())
-  // Handle options credentials check - before CORS!
-  // and fetch cookies credentials requirement
-
-  .use(express.urlencoded({ extended: false }))
-  // .use(morgan("dev"))
+  // .use(express.urlencoded({ extended: false }))
+  .use(morgan('dev'))
   .use(express.json())
 
   .use(indexRoutes)
-  .use("/api", usersRoutes)
-  .use("/api", cartRoutes)
-  .use("/api", payRoutes)
-  .use("/api", orderRoutes)
-  .use("/api", likesRoutes);
+  .use('/api', usersRoutes)
+  .use('/api', cartRoutes)
+  .use('/api', payRoutes)
+  .use('/api', orderRoutes)
+  .use('/api', likesRoutes);
 
 // .use(errorHandler);
 export default app;
