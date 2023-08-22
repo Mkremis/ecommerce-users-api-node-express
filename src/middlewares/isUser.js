b373ea8be39d4622431bd54c48e4341acd017f12
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-22 09:46:13 -0300

    1 file modified

--- src/middlewares/isUser.js
+++ src/middlewares/isUser.js
@@ -1,18 +1,20 @@
 import { pool } from '../db.js';
 
 const isUser = async (req, res, next) => {
-  const { login_username, login_password } = req.body;
-  // if (!login_username || !login_password)
-  //   return res
-  //     .status(400)
-  //     .json({ message: "username and password are required." });
+  try {
+    const { login_username, login_password } = req.body;
+    if (!login_username || !login_password) {
+      throw new Error({ message: ['username and password are required.'] });
+    }
+    let [rows] = await pool.query(
+      `SELECT login_password FROM users WHERE login_username = ?`,
+      [login_username]
+    );
 
-  let [rows] = await pool.query(
-    `SELECT login_password FROM users WHERE login_username = ?`,
-    [login_username]
-  );
-
-  if (rows.length > 0) req.passwordHash = rows[0].login_password;
-  next();
+    if (rows.length > 0) req.passwordHash = rows[0].login_password;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 export { isUser };
