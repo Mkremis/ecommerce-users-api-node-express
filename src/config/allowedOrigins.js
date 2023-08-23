df82c92f1fa066417b51151db5428d91e65e547f
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-23 10:01:27 -0300

    1 file modified

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 export const allowedOrigins = [
   "https://localhost:3000",
   "https://mkremis.github.io",
-  "https://wmryv5-5173.csb.app",
+  "https://9sg732-5173.csb.app",
   "http://localhost:5173",
 ];
