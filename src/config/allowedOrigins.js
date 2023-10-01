5a4792b72746b1b0cc3af474db72902f6e8e0176
Mkremis <martinkremis@hotmail.com>
2023-10-01 12:29:41 -0300

    allowedOrigins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,4 @@
 export const allowedOrigins = [
   "https://localhost:3000",
   "https://mkremis.github.io",
-  "https://vigilant-pancake-gvgwrqvrw5rcwpjr-5173.app.github.dev",
-  "http://localhost:5173",
 ];
