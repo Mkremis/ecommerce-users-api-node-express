bd3d21ccfe4c2185eacf8c142a030680ad389f22
Mkremis <martinkremis@hotmail.com>
2024-04-02 17:31:39 -0300

    payment test

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,5 +1,5 @@
 // Step 1: Import the parts of the module you want to use
-import { MercadoPagoConfig, Preference } from "mercadopago";
+import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
 import { MERCADOPAGO_API_KEY } from "../config.js";
 
 import db from "../index.js";
@@ -19,10 +19,9 @@ export const createOrderController = async (req, res) => {
       // Step 2: Initialize the client object
       const client = new MercadoPagoConfig({
         accessToken: MERCADOPAGO_API_KEY,
-        options: { timeout: 5000 },
       });
 
-      const preference = new Preference(client);
+      //  const preference = new Preference(client);
       // Step 3: Create an item object
       const items = order.map(
         ({
@@ -46,29 +45,54 @@ export const createOrderController = async (req, res) => {
           };
         }
       );
-      preference
-        .create({
-          body: {
-            notification_url: `${URL}/api/users/webhook`,
-            back_urls: {
-              success: `${URL}/api/users/success`,
-              failure: `${URL}/api/users/failure`,
-              pending: `${URL}/api/users/pending`,
-            },
-            payer: {
-              email: email,
-              surname: userName,
-            },
-            items,
+      const payment = new Payment(client);
+
+      const body = {
+        items,
+        token: "token",
+        description: "description",
+        installments: 1,
+        payment_method_id: "visa",
+        notification_url: `${URL}/api/users/webhook`,
+        payer: {
+          email,
+          identification: {
+            type: "CPF",
+            number: "19119119100",
           },
+        },
+      };
+
+      payment
+        .create({
+          body: body,
+          requestOptions: { idempotencyKey: "<SOME_UNIQUE_VALUE>" },
         })
-        .then((response) => {
-          res.status(200).json({ payLink: response.init_point });
-        })
-        .catch((error) => {
-          console.error("Error creating preference:", error);
-          res.status(500).json({ error: "Error creating preference" });
-        });
+        .then(console.log)
+        .catch(console.log);
+      // preference
+      //   .create({
+      //     body: {
+      //       notification_url: `${URL}/api/users/webhook`,
+      //       back_urls: {
+      //         success: `${URL}/api/users/success`,
+      //         failure: `${URL}/api/users/failure`,
+      //         pending: `${URL}/api/users/pending`,
+      //       },
+      //       payer: {
+      //         email: email,
+      //         surname: userName,
+      //       },
+      //       items,
+      //     },
+      //   })
+      //   .then((response) => {
+      //     res.status(200).json({ payLink: response.init_point });
+      //   })
+      //   .catch((error) => {
+      //     console.error("Error creating preference:", error);
+      //     res.status(500).json({ error: "Error creating preference" });
+      //   });
     } else {
       res.status(400).json({ error: "Invalid request or empty order" });
     }
